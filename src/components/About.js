3d37c83e3089d1a76a0de544eb42671925f35357
SaintJul <favor.julius@gmail.com>
2021-09-30 18:28:11 -0400

    commit tree 2b8419e50fc3752a7458de5c28c0647a2ce62aed

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id = "about">
+    <h2>About Me</h2>
+    <p>Love playing me some soccer</p>
+    <img src = {image} alt = "I made this"/>
+  </div>
+  )
 }
 
 export default About;
